1a560b260f7d931b5ae835587d78f0d4f00bf9ec
Saikiran Vaidhyanathan <saikiran.vaidhyanathan@sjsu.edu>
2021-04-29 13:22:45 -0700

    added table view

--- frontend/src/components/student.js
+++ frontend/src/components/student.js
@@ -6,48 +6,66 @@ import './Student.css';
 
 export default class Student extends Component {
 
-    render() {
+  render() {
 
 
-                function success() {
-                var skills = document.getElementById('skills').value;
-                var work = document.getElementById('work').value;
-                var project = document.getElementById('projects').value;
-                alert("Updated skills: " + skills)
-                alert("Updated work experience: " + work)
-                alert("Updated personal projects: " + project)
-                }
-            
-        document.title = 'Student page'
-
-        var CurrentDate = new Date().toLocaleDateString(); //Uses React's in built date function 
-
-        return (
-            
-                    
-            <form>
-            <br></br>
-            <div class="student-input">
-               <h1 class="top">Elias Faris </h1>
-              <span class="input-group-addon">Skills</span>
-              <input id="skills" type="text" class="form-control" name="msg" placeholder="All Skills (ex. Python, JavaScript, ...)"/>
-              <br></br>
-              <span class="input-group-addon">Work Experience</span>
-              <input id="work" type="text" class="form-control" name="msg" placeholder="Describe experiences. Separate each as a bullet point"/>
-              <br></br>
-              <span class="input-group-addon">Personal Project</span>
-              <input id="project" type="text" class="form-control" name="msg" placeholder="Describe projects you've done. Separate each as a bullet point"/>
-            </div>
-            
-            <center>
-                <button type="submit" class="btn btn-primary" onclick={success}>Save</button>
-            </center>
-          </form>
-                    
-
-
-                
-        );
+    function success() {
+      var skills = document.getElementById('skills').value;
+      var work = document.getElementById('work').value;
+      var project = document.getElementById('projects').value;
+      alert("Updated skills: " + skills)
+      alert("Updated work experience: " + work)
+      alert("Updated personal projects: " + project)
     }
+
+    // console.log(skills);
+
+    document.title = 'Student page'
+
+    var CurrentDate = new Date().toLocaleDateString(); //Uses React's in built date function 
+
+    return (
+      <div>
+        <br></br>
+        <center>
+          <table class="studentInfo">
+            <tr>
+              <th>Tools</th>
+
+              <th></th>
+            </tr>
+            <tr>
+              <td></td>
+              <td>{
+                <form>
+                  <br></br>
+                  <div class="student-input">
+                    <h1 class="top">Elias Faris </h1>
+                    <span class="input-group-addon">Skills</span>
+                    <input id="skills" type="text" class="form-control" name="msg" placeholder="All Skills (ex. Python, JavaScript, ...)" />
+                    <br></br>
+                    <span class="input-group-addon">Work Experience</span>
+                    <input id="work" type="text" class="form-control" name="msg" placeholder="Describe experiences. Separate each as a bullet point" />
+                    <br></br>
+                    <span class="input-group-addon">Personal Project</span>
+                    <input id="project" type="text" class="form-control" name="msg" placeholder="Describe projects you've done. Separate each as a bullet point" />
+                  </div>
+                  <br>
+                  </br>
+                  <center>
+                    <button type="submit" class="btn btn-primary" onclick={success}>Save</button>
+                  </center>
+
+                </form>}
+              </td>
+            </tr>
+          </table>
+        </center>
+      </div>
+
+
+
+    );
+  }
 
 }
